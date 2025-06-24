106d1e0ab2b33ed95ab305e75e56bb3453a31d8f
Theomrh <theo.mrhd@gmail.com>
2025-06-24 09:03:06 +0200

    git init

---
+++ app/+not-found.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function NotFound() {
+	return (
+		<View className="flex flex-1 items-center justify-center bg-background p-4 gap-y-4">
+			<Text>Hello</Text>
+		</View>
+	);
+}
