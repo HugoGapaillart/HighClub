a0a54ca28a11618bc28653f0460f907a64e05888
Theomrh <theo.mrhd@gmail.com>
2025-06-24 11:47:39 +0200

    feat(tabs): add events and tickets screens with tab navigation
    - Add new events and tickets screens with basic UI
    - Implement tab navigation with icons using lucide-react-native
    - Remove unused modal screen and clean up layout files
    - Update package.json to include lucide-react-native dependency

--- app/(protected)/(tabs)/_layout.tsx
+++ app/(protected)/(tabs)/_layout.tsx
@@ -1,31 +1,47 @@
 import React from "react";
 import { Tabs } from "expo-router";
-
-import { useColorScheme } from "@/lib/useColorScheme";
-import { colors } from "@/constants/colors";
+import { House, Calendar, Tickets, User } from "lucide-react-native";
 
 export default function TabsLayout() {
-	const { colorScheme } = useColorScheme();
-
-	return (
-		<Tabs
-			screenOptions={{
-				headerShown: false,
-				tabBarStyle: {
-					backgroundColor:
-						colorScheme === "dark"
-							? colors.dark.background
-							: colors.light.background,
-				},
-				tabBarActiveTintColor:
-					colorScheme === "dark"
-						? colors.dark.foreground
-						: colors.light.foreground,
-				tabBarShowLabel: false,
-			}}
-		>
-			<Tabs.Screen name="index" options={{ title: "Home" }} />
-			<Tabs.Screen name="settings" options={{ title: "Settings" }} />
-		</Tabs>
-	);
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: true,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Accueil",
+          tabBarIcon: ({ size, color }) => <House size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="events"
+        options={{
+          title: "Évènements",
+          tabBarIcon: ({ size, color }) => (
+            <Calendar size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="tickets"
+        options={{
+          title: "Mes tickets",
+          tabBarIcon: ({ size, color }) => (
+            <Tickets size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Profil",
+          tabBarIcon: ({ size, color }) => <User size={size} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
---
+++ app/(protected)/(tabs)/tickets.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "@/components/ui/text";
+
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background p-4 gap-y-4">
+      <Text>Mes tickets</Text>
+    </View>
+  );
+}
